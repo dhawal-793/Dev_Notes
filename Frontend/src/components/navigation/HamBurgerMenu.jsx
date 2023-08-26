d08b4d262d70906dcd034f1fe9b04a5cb2885dec
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-26 19:33:24 +0530

    Created HamBurgerMenu component yo show menu icons

---
+++ Frontend/src/components/navigation/HamBurgerMenu.jsx
@@ -0,0 +1,32 @@
+import { Hamburger, Menu, Cancel } from '../Icons';
+const HamBurgerMenu = ({ handleClick, navOpen }) => {
+    return (
+
+        <div className='z-50 text-gray-500 transition-all duration-300 ease-in-out cursor-pointer group md:hidden hover:scale-110 hover:text-gray-300'
+            onClick={handleClick}
+        >
+            {navOpen &&
+                <div className='group'>
+                    <span className="group-hover:hidden ">
+                        <Cancel color="#6B7280" />
+                    </span>
+                    <span className="hidden group-hover:block">
+                        <Cancel color="#EF4444" />
+                    </span>
+                </div>
+            }
+            {!navOpen &&
+                <div>
+                    <span className="group-hover:hidden ">
+                        <Menu />
+                    </span>
+                    <span className="hidden transition-all delay-75 group-hover:block">
+                        <Hamburger color="#6B7280"/>
+                    </span>
+                </div>
+            }
+        </div>
+    )
+}
+
+export default HamBurgerMenu;
