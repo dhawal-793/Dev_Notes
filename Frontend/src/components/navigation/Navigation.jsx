8250f6fe0fde30fb014216b3fbd216e715405306
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-27 20:47:48 +0530

    Added class for currently active page link.

--- Frontend/src/components/navigation/Navigation.jsx
+++ Frontend/src/components/navigation/Navigation.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import AuthLinks from './AuthLinks';
 
 const navLinks = [
@@ -21,13 +21,17 @@ const navLinks = [
 
 
 const Navigation = ({ ulClass, liClass, handleClick }) => {
+
+    const location = useLocation()
     return (
         <ul className={ulClass}>
             <ul className='flex flex-col items-center justify-start flex-1 gap-2 md:gap-5 md:flex-row'>
                 {navLinks.map(({ id, link, name }) => {
+
+                    const isActive = location.pathname === link;
                     return (
                         <li key={id}
-                            className={`md:px-2 capitalize font-medium text-lg cursor-pointer text-gray-500 duration-500 hover:text-gray-300 ${liClass}`}>
+                            className={`md:px-2 capitalize font-medium text-lg cursor-pointer ${isActive ? "text-gray-300" : "text-gray-500"} duration-500 hover:text-gray-300 ${liClass}`}>
                             <Link className='hover:text-gray-300'
                                 onClick={handleClick}
                                 to={link}> {name}
