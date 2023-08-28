b7663f0b05e5474ba5b8027c890f313083c95217
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-28 14:55:58 +0530

    Updated extension from js to ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -6,9 +6,9 @@ export default defineConfig(() => {
     build: {
       outDir: 'build',
     },
-      plugins: [react()],
-      server: {
-        port: 3000,
-      },
+    plugins: [react()],
+    server: {
+      port: 3000,
+    },
   };
 });
