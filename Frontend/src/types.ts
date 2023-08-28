097e79e6fb9187a99488bfea86ce495ebf7653c9
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-28 15:10:03 +0530

    Created Types for different types of  Notes.

---
+++ Frontend/src/types.ts
@@ -0,0 +1,17 @@
+export interface Note {
+  _id:string
+  title: string
+  description: string
+  tag: string
+}
+export interface NewNote {
+  title: string
+  description: string
+  tag: string
+}
+export interface EditedNote {
+  id:string
+  edittitle: string
+  editdescription: string
+  edittag: string
+}
