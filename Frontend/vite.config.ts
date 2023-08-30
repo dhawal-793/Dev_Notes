6e147fc738d6cdbd60aeb0a589b33b16b34f51e5
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-30 08:12:10 +0530

    Updated alias for imports.

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite';
+import path from "path"
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
-    },
-    plugins: [react()],
-    server: {
-      port: 3000,
-    },
-    resolve: {
-      alias: [
-        { find: /^@src/, replacement: '/src' },
-        // Add more alias mappings as needed
-      ]
+export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      // { find: /^@src/, replacement: '/src' },
+      // Add more alias mappings as needed
     }
-  };
+  }
 });
