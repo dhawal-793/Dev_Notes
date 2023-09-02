3495b352d0818d044107fe1821e8f60882a88986
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-09-02 19:52:48 +0530

    Updated Frontend URL for development.

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["http://localhost:3000", process.env.FRONTEND],
+    origin: [process.env.FRONTEND_DEV, process.env.FRONTEND],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   }
 ));
