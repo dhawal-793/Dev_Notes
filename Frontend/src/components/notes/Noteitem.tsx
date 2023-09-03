e59ac532b511ed5f92efa566aaf52628fa7136d6
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-09-03 14:06:40 +0530

    Added confirm alert on deleting a note using AlertModal.

--- Frontend/src/components/notes/Noteitem.tsx
+++ Frontend/src/components/notes/Noteitem.tsx
@@ -1,7 +1,8 @@
-import { FC, useContext } from "react";
+import { FC, useState, useContext } from "react";
 
 import noteContext from "@/context/Notes/noteContext";
 import { Note } from "@/types";
+import AlertModal from "../modals/alert-modal";
 
 interface NoteitemProps {
   note: Note;
@@ -9,75 +10,63 @@ interface NoteitemProps {
 }
 
 const Noteitem: FC<NoteitemProps> = ({ note, updateNote }) => {
-  /* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
-  /* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
-
-  // VARIABLES
-
   const context = useContext(noteContext);
   const { deleteNote } = context;
-  /* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
-  /* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
-
-  // RETURN
+  const [open, setOpen] = useState(false)
 
   return (
-    <div className="container ">
-      <div className="py-3 mb-5 rounded card custom-shadow bg-body">
-        <div
-          style={{
-            display: "flex",
-            position: "absolute",
-            justifyContent: "flex-end",
-            left: "21px",
-          }}
-        >
-          <span className="badge bg-danger">
-            <i className="fa-solid fa-tag cursorPointer"></i> {note.tag}
-          </span>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            position: "absolute",
-            justifyContent: "flex-end",
-            right: "40px",
-          }}
-        >
-          <i
-            className="mx-2 fa-regular fa-pen-to-square cursorPointer"
-            onClick={() => {
-              updateNote(note);
+    <>
+      <AlertModal isOpen={open} onClose={() => setOpen(false)} onConfirm={() => deleteNote(note._id)} />
+      <div className="container ">
+        <div className="py-3 mb-5 rounded card custom-shadow bg-body">
+          <div
+            style={{
+              display: "flex",
+              position: "absolute",
+              justifyContent: "flex-end",
+              left: "21px",
             }}
-          ></i>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            position: "absolute",
-            justifyContent: "flex-end",
-            right: "10px",
-          }}
-        >
-          <i
-            className="mx-2 fa-regular fa-trash-can cursorPointer"
-            onClick={() => {
-              deleteNote(note._id);
+          >
+            <span className="badge bg-danger">
+              <i className="fa-solid fa-tag cursorPointer"></i> {note.tag}
+            </span>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              position: "absolute",
+              justifyContent: "flex-end",
+              right: "40px",
             }}
-          ></i>
-        </div>
-        <div className="my-2 card-body">
-          <h4 className="my-2 card-title">{note.title}</h4>
-          <p className="card-text">{note.description}</p>
+          >
+            <i
+              className="mx-2 fa-regular fa-pen-to-square cursorPointer"
+              onClick={() => {
+                updateNote(note);
+              }}
+            ></i>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              position: "absolute",
+              justifyContent: "flex-end",
+              right: "10px",
+            }}
+          >
+            <i
+              className="mx-2 fa-regular fa-trash-can cursorPointer"
+              onClick={() => setOpen(true)}
+            ></i>
+          </div>
+          <div className="my-2 card-body">
+            <h4 className="my-2 card-title">{note.title}</h4>
+            <p className="card-text">{note.description}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
-
-/* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
-/* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
-
-// EXPORT
 
 export default Noteitem;
