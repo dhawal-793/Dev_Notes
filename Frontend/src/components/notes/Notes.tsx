4d9ab6683d9a7b0fcd58ad3f63223501420e28fc
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-09-03 14:05:44 +0530

    Updated import for NoteModal.

--- Frontend/src/components/notes/Notes.tsx
+++ Frontend/src/components/notes/Notes.tsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { Button } from "@/components/ui/button";
-import NoteModal from "@/components/NoteModal";
+import NoteModal from "@/components/modals/note-modal";
 import noteContext from "@/context/Notes/noteContext";
 import { Note } from "@/types";
 
