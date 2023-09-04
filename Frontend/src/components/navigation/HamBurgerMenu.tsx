227094bac7f8a68942251f23ef74a0e7a85cea80
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-09-04 12:46:12 +0530

    Updated HamburgerMenu to use lucide Icons.

--- Frontend/src/components/navigation/HamBurgerMenu.tsx
+++ Frontend/src/components/navigation/HamBurgerMenu.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
+import { List,  X } from 'lucide-react';
 
-import { Hamburger, Menu, Cancel } from '@/components/Icons';
 import { Button } from '@/components/ui/button';
 
 interface HamBurgerMenuProps {
@@ -10,27 +10,9 @@ interface HamBurgerMenuProps {
 
 const HamBurgerMenu: FC<HamBurgerMenuProps> = ({ handleClick, navOpen }) => {
     return (
-        <Button variant="ghost" size="icon" onClick={handleClick} className='z-50 group md:hidden'>
-            {navOpen &&
-                <div className='group'>
-                    <span className="group-hover:hidden ">
-                        <Cancel color="#6B7280" />
-                    </span>
-                    <span className="hidden group-hover:block">
-                        <Cancel color="#EF4444" />
-                    </span>
-                </div>
-            }
-            {!navOpen &&
-                <div>
-                    <span className="group-hover:hidden ">
-                        <Menu />
-                    </span>
-                    <span className="hidden transition-all delay-75 group-hover:block">
-                        <Hamburger color="#6B7280" />
-                    </span>
-                </div>
-            }
+        <Button variant="ghost" size="icon" onClick={handleClick} className='z-50 md:hidden'>
+            <List className={`h-[1.2rem] w-[1.2rem] transition-all ${!navOpen ? "rotate-0 scale-100" : " -rotate-180 scale-0 "}`} />
+            <X className={`absolute h-[1.2rem] w-[1.2rem]  transition-all ${navOpen ? "rotate-0 scale-100" : "rotate-180 scale-0"}`} />
         </Button>
     )
 }
