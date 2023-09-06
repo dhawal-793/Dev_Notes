f92699a562302b69252cf6afc1f6fcc94575b841
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-09-06 21:56:09 +0530

    Added tooltip for Edit and Delete icon buttons.

--- Frontend/src/components/notes/Noteitem.tsx
+++ Frontend/src/components/notes/Noteitem.tsx
@@ -4,6 +4,7 @@ import { Edit, Trash } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import AlertModal from "@/components/modals/alert-modal";
+import ToolTipBox from "@/components/ui/tool-tip-box";
 import noteContext from "@/context/Notes/noteContext";
 import { Note } from "@/types";
 
@@ -30,12 +31,16 @@ const Noteitem: FC<NoteitemProps> = ({ note, updateNote }) => {
               </Badge>
             </div>
             <div className="flex items-center gap-1">
-              <button onClick={() => updateNote(note)} className="cursor-pointer w-8 h-8 relative flex select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
-                <Edit size={20} />
-              </button>
-              <button onClick={() => setOpen(true)} className="cursor-pointer w-8 h-8 relative flex select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
-                <Trash size={20} />
-              </button>
+              <ToolTipBox tip="Edit Note">
+                <button onClick={() => updateNote(note)} className="cursor-pointer w-8 h-8 relative flex select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
+                  <Edit size={20} />
+                </button>
+              </ToolTipBox>
+              <ToolTipBox tip="Delete Note">
+                <button onClick={() => setOpen(true)} className="cursor-pointer w-8 h-8 relative flex select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
+                  <Trash size={20} />
+                </button>
+              </ToolTipBox>
 
             </div>
           </div>
