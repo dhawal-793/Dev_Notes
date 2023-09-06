82f546c183ea2240bfde5ed140efd8bfb40332ff
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-09-06 22:30:40 +0530

    Added tooltip for Dialog close  button.

--- Frontend/src/components/ui/dialog.tsx
+++ Frontend/src/components/ui/dialog.tsx
@@ -4,6 +4,8 @@ import { X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
+import ToolTipBox from "./tool-tip-box"
+
 const Dialog = DialogPrimitive.Root
 
 const DialogTrigger = DialogPrimitive.Trigger
@@ -46,10 +48,13 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-        <X className="w-4 h-4" />
-        <span className="sr-only">Close</span>
-      </DialogPrimitive.Close>
+      <ToolTipBox tip="Close Dialog">
+
+        <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+          <X className="w-4 h-4" />
+          <span className="sr-only">Close</span>
+        </DialogPrimitive.Close>
+      </ToolTipBox>
     </DialogPrimitive.Content>
   </DialogPortal>
 ))
