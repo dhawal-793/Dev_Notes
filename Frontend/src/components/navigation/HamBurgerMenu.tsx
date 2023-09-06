87f6f38237a147918a1e55b0093396c298648ac8
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-09-06 21:58:13 +0530

    Added tooltip for Menu icon buttons.

--- Frontend/src/components/navigation/HamBurgerMenu.tsx
+++ Frontend/src/components/navigation/HamBurgerMenu.tsx
@@ -1,7 +1,8 @@
 import { FC } from 'react';
-import { List,  X } from 'lucide-react';
+import { List, X } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
+import ToolTipBox from '@/components/ui/tool-tip-box';
 
 interface HamBurgerMenuProps {
     handleClick: () => void;
@@ -11,8 +12,12 @@ interface HamBurgerMenuProps {
 const HamBurgerMenu: FC<HamBurgerMenuProps> = ({ handleClick, navOpen }) => {
     return (
         <Button variant="ghost" size="icon" onClick={handleClick} className='z-50 md:hidden'>
-            <List className={`h-[1.2rem] w-[1.2rem] transition-all ${!navOpen ? "rotate-0 scale-100" : " -rotate-180 scale-0 "}`} />
-            <X className={`absolute h-[1.2rem] w-[1.2rem]  transition-all ${navOpen ? "rotate-0 scale-100" : "rotate-180 scale-0"}`} />
+            <ToolTipBox tip="Open Menu">
+                <List className={`h-[1.2rem] w-[1.2rem] transition-all ${!navOpen ? "rotate-0 scale-100" : " -rotate-180 scale-0 "}`} />
+            </ToolTipBox>
+            <ToolTipBox tip="Close Menu">
+                <X className={`absolute h-[1.2rem] w-[1.2rem]  transition-all ${navOpen ? "rotate-0 scale-100" : "rotate-180 scale-0"}`} />
+            </ToolTipBox>
         </Button>
     )
 }
