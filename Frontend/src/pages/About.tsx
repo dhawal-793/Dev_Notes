6221e1319d4b04e4720f3a7092c80c50368e2039
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-09-07 23:36:08 +0530

    Updated About Page.

--- Frontend/src/pages/About.tsx
+++ Frontend/src/pages/About.tsx
@@ -1,74 +1,49 @@
-const About = () => {
-  /* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
-  /* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 
-  // RETURN
+
+const messages = [
+  `DevNotes is a cloud based Notes app build to store user's notes anytime, anywhere on the cloud with easy access to their notes.`,
+  `The App is build using the popular technologies like React JS MongoDB, Express Js and NodeJS.`,
+  `Tailwind is Used as the CSS Framework for styling the App.`,
+  `shadcn-ui is used as the component library for the App with radix-ui.`,
+  `The App is completely TypeSafe with Typescript.`,
+  `ALl the forms are completely typeSafe with ZOD and react-hook-form.`,
+  `Vite JS is used as the bundler`,
+  `For fetching the notes of the logged in user Custom Express APIs are used.`,
+]
+
+const features = [
+  `The app uses Express Api to fetch all the data.`,
+  ` MongoDb is used as the database to save and fetch all the
+  data.`,
+  `The user can create read update and delete notes on the app.`,
+  `The app uses function based react components.`,
+  `It is completely Device Responsive and accessibilty optimized`,
+
+]
+
+const About = () => {
   return (
-    <div className="text-white card bg-custom">
-      <div className="card-body">
-        <h3 className="py-2 text-center card-title">DevNotes</h3>
-        <hr />
-        <ul style={{ listStyleType: "square" }}>
-          <li>
-            <h5>
-              DevNotes is a cloud based Notes app build to store user's notes
-              anytime, anywhere on the cloud with easy access to their
-              notes.
-            </h5>
-          </li>
-          <li>
-            <h5>
-              The App is build using the latet technologies like MERN Stack.
-            </h5>
-          </li>
-          <li>
-            <h5>
-              Tailwind is Used as the CSS Framework for styling the App.
-            </h5>
-          </li>
-          <li>
-            <h5>
-              For fetching the notes of the logged in user Custom Express
-              APIs are used.
-            </h5>
-          </li>
-          <li>
-            <h5>Some Features of DevNotes &rarr;</h5>
-            <ul>
-              <li>The app uses Express Api to fetch all the data.</li>
-              <li>
-                MongoDb is used as the database to save and fetch all the
-                data.
-              </li>
-              <li>
-                The user can create read update and delete notes on the app.
-              </li>
-              <li>
-                The user can only access his/her own notes, no-one else can
-                see other user's notes, thus the app is secured.
-              </li>
-              {/* <li>
-                  The user can also search their notes using the tag assigned
-                  to each note.
-                </li> */}
-              <li>The app uses function based react components.</li>
-              {/* <li>
-                    Some features like Top Loading Bar and Infinite Scroll are
-                    also Included.
-                  </li> */}
-              <li>It is completely Device Responsive</li>
-              <li>All the Rights are reserved by DevNotes. </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
+    <div className="container py-10">
+      <Heading title="DevNotes" description="Your Notes Secured on the Cloud" className="text-center" />
+      <Separator className="mt-5 mb-8" />
+      <ul className="flex flex-col max-w-4xl gap-3 mx-auto list-[square]">
+        {messages.map((message, i) => (
+          <li key={i} className="text-lg font-semibold tracking-wider">{message}</li>
+        ))}
+
+        <li className="text-lg font-semibold tracking-wider">
+          Some Features of DevNotes
+          <ul className="flex flex-col gap-2 px-5 py-3 text-base list-disc">
+            {features.map((feature, i) => (
+              <li key={i} className="text-lg font-semibold tracking-wider">{feature}</li>
+            ))}
+          </ul>
+        </li>
+      </ul>
     </div>
   );
 };
-
-/* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
-/* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
-
-// EXPORT
 
 export default About;
